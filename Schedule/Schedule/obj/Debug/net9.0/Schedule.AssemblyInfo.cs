1f837aa067c9e66016323fcfc8527e2a0e799a32
max <m.moser@htlkrems.at>
2026-04-23 12:07:46 +0200

    wrote component for entries

--- Schedule/Schedule/obj/Debug/net9.0/Schedule.AssemblyInfo.cs
+++ Schedule/Schedule/obj/Debug/net9.0/Schedule.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Schedule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22714823d8f412b50ffe9240444a362349837d7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61db1db27ebe72968a16c0c2c11cdad6e64cad42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Schedule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Schedule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Schedule/Stundenplan/obj/Debug/net9.0/Stundenplan.AssemblyInfo.cs
+++ Schedule/Stundenplan/obj/Debug/net9.0/Stundenplan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stundenplan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22714823d8f412b50ffe9240444a362349837d7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61db1db27ebe72968a16c0c2c11cdad6e64cad42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stundenplan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stundenplan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
